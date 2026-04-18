9b6d30a30fb03f8b0129104fd5d0e1a8d76c06f9
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2026-04-18 20:59:03 +0600

    2nd commit

--- D_Positions_in_array.c
+++ D_Positions_in_array.c
@@ -23,9 +23,19 @@ int main()
     scanf("%d", &n);
     int a[n];
 
-    
-    
-      
-      
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] <= 10)
+        {
+            printf("A[%d] = %d\n", i, a[i]);
+        }
+    }
+
     return 0;
 }
